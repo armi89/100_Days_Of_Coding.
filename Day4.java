db3c0a5107fb5aaf08a8f4165fca3cbdc9591b18
Armi <arm844630@gmail.com>
2023-10-12 17:16:21 +0800

    Day4

---
+++ Day4.java
@@ -0,0 +1,12 @@
+
+public class Day4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int hasil = a + b;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
+
+
