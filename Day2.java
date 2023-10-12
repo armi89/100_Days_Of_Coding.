5ac0674cddcc1c11931f59f7036f2372fa25deed
Armi <arm844630@gmail.com>
2023-10-12 17:08:15 +0800

    first commit

---
+++ Day2.java
@@ -0,0 +1,9 @@
+
+public class Day2 {
+      public static void main(String[] args) {
+        String nama = "Armi";
+        int umur = 20;
+        System.out.println("nama saya " + nama );
+        System.out.println("umur saya " + umur + " tahun ");
+    }
+}
---
+++ Day3.java
@@ -0,0 +1,9 @@
+
+public class Day3 {
+     // Program sederhana untuk menampilkan "Hello, world!"
+    public static void main(String[] args) {
+
+        // Menampilkan pesan "Hello, world!"
+        System.out.println("Hello, world!");
+    }
+}
