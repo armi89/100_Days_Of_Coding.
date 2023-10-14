a61cc35cad7633d54b85d5674f54ec1b6c81ffc1
Armi <arm844630@gmail.com>
2023-10-14 19:38:31 +0800

    Day5

---
+++ Day5.java
@@ -0,0 +1,17 @@
+
+public class Day5 {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 4;
+
+        int hasil = angka1 + angka2;
+
+        System.out.println("Hasil penjumlahan adalah: " + hasil);
+        System.out.println("Hasil dari pengurangan adalah: " + (angka1 - angka2));
+
+    }
+}
+    
+            
+    
+
