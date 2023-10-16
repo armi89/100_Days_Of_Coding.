646afd06f5a0cae164026375d2dd041253df96a4
Armi <arm844630@gmail.com>
2023-10-16 19:12:26 +0800

    Day7

---
+++ Day7.java
@@ -0,0 +1,14 @@
+
+public class Day7 {
+    public static void main(String[] args) {
+        // Deklarasikan variabel untuk menyimpan panjang dan lebar persegi
+        int panjang = 10;
+        int lebar = 5;
+
+        // Menghitung luas persegi
+        int luas = panjang * lebar;
+
+        // Tampilkan luas persegi
+        System.out.println("Luas persegi: " + luas);
+    }
+}
