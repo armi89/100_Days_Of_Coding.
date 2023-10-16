543a6a2b93ec53ed4a788ad6eed4a9e8f15a0b27
Armi <arm844630@gmail.com>
2023-10-16 21:26:12 +0800

    Day8

---
+++ Day8.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class Day8 {
+    public static void main(String[] args) {
+        // Buat objek scanner
+        Scanner input = new Scanner(System.in);
+
+        // Minta input dari pengguna
+        System.out.print("Masukkan angka: ");
+        int angka = input.nextInt();
+
+        // Tampilkan input dari pengguna
+        System.out.println("Angka yang Anda masukkan adalah " + angka);
+    }
+    }
+    
+
