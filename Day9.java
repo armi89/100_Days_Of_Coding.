8ae4c2cff2dc114a76afe5a8843fe30a4216dbe9
Armi <arm844630@gmail.com>
2023-10-18 19:30:59 +0800

    Day9

---
+++ Day9.java
@@ -0,0 +1,17 @@
+
+public class Day9 {
+    public static void main(String[] args) {
+         // Tipe data byte
+        byte nilaiByte = 127;
+        System.out.println("Nilai byte: " + nilaiByte);
+
+        // Tipe data short
+        short nilaiShort = 32767;
+        System.out.println("Nilai short: " + nilaiShort);
+
+        // Tipe data int
+        int nilaiInt = 2147483647;
+        System.out.println("Nilai int: " + nilaiInt);
+    }
+}
+
