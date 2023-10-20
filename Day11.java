a7409ef7b44b70f8736db55aa4049c1774917f78
Armi <arm844630@gmail.com>
2023-10-20 21:53:42 +0800

    perhitungan pembagian

---
+++ Day11.java
@@ -0,0 +1,15 @@
+
+public class Day11 {
+    public static void main(String[] args) {
+        //Deklarasi variabel
+        int a=20;
+        int b=10;
+        
+        //melakukan pembagian
+        int hasil=a/b;
+        
+        //menampilkan hasil 
+        System.out.println("Hasil pembagian:"+hasil);
+    }
+    
+}
