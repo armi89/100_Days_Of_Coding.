fcae3855d7ab6f251d79faa457b1ce45d39e9551
Armi <arm844630@gmail.com>
2023-10-21 20:31:14 +0800

    pengurangan dengan variabel desimal

---
+++ Day13.java
@@ -0,0 +1,17 @@
+
+public class Day13 {
+    public static void main(String[] args) {
+         // Deklarasi variabel
+        double a = 10.5;
+        double b = 5.25;
+
+        // Pengurangan
+        double hasil = a - b;
+
+        // Output
+        System.out.println("Hasil pengurangan: " + hasil);
+    }
+}
+    
+    
+
