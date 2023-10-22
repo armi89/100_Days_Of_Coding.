2e0d03ca95f88961045ba3cdc5b5972fc1bc0355
Armi <arm844630@gmail.com>
2023-10-22 18:42:31 +0800

    menampilkan angka 20 hingga angka 100 menggunakan perulangan for

---
+++ Day14.java
@@ -0,0 +1,11 @@
+
+public class Day14 {
+    public static void main(String[] args) {
+        int i = 10;
+        for (i = 10; i <= 100; i++) {
+            // Output
+            System.out.println(i);
+        }
+    }
+}
+
