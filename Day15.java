2a2ce614a0ed3e663a49e18396e891dbba97c355
Armi <arm844630@gmail.com>
2023-10-22 18:50:50 +0800

    menghitung voulume kubus

---
+++ Day15.java
@@ -0,0 +1,11 @@
+
+public class Day15 {
+    public static void main(String[] args) {
+        double sisi = 5;
+
+    double volume = sisi * sisi * sisi;
+
+    System.out.println("Volume kubus adalah: " + volume);
+    }
+    
+}
