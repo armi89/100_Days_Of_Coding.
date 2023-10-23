e6fd737661c724e9640522f761aee57ed9d45886
Armi <arm844630@gmail.com>
2023-10-23 19:29:12 +0800

    mengimput nama dan nilai

---
+++ Day16.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class Day16 {
+    public static void main(String[] args) {
+        String nama;
+        int nilai;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nama: ");
+        nama = input.nextLine();
+        System.out.print("Masukkan nilai: ");
+        nilai = input.nextInt();
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+    }
+}
+
+
+
