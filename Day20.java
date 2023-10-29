aacf57ef0b2e63ae41df9ace473be8239ad32061
Armi <arm844630@gmail.com>
2023-10-30 05:58:38 +0800

    menampilkan nilai ganjil

--- Day20.java
+++ Day20.java
@@ -1,5 +1,6 @@
 
 public class Day20 {
+
     public static void main(String[] args) {
         int[] numbers = {3, 5, 8, 9, 10};
         int minValue = numbers[0];
@@ -13,7 +14,3 @@ public class Day20 {
         System.out.println("Nilai terkecil: " + minValue);
     }
 }
-
-    
-  
-
---
+++ Day21.java
@@ -0,0 +1,14 @@
+
+public class Day21 {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        // Menampilkan bilangan ganjil
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
