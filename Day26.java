d562f5a017c76789cb42f2286e0a8f0d61d26707
Armi <arm844630@gmail.com>
2023-11-03 20:40:36 +0800

    mencetak nilai acak

---
+++ Day26.java
@@ -0,0 +1,12 @@
+
+import java.util.Random;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt();
+        System.out.println(number);
+
+    }
+}
