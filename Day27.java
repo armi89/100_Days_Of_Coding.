67651977308721fce447314bacbd51372b7d9ede
Armi <arm844630@gmail.com>
2023-11-05 07:43:30 +0800

    menampilkan pesan atau masukkan bilangan

---
+++ Day27.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("masukkan bilangan:");
+        int bilangan = scanner.nextInt();
+        System.out.println("Bilangan yang anda masukkan adalah " + bilangan);
+
+    }
+
+}
