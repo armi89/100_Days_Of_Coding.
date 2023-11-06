14d4c296879396ef999e25946bed0b0892d4bda9
Armi <arm844630@gmail.com>
2023-11-06 22:58:47 +0800

    menampilkan data mahasiswa

---
+++ Day28.java
@@ -0,0 +1,13 @@
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        String nama = "armikamil";
+        int nim =0221057;
+        String jurusan = "Teknik Informatika";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
