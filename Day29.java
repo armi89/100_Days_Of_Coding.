6687d070c246c8743cebee1e1c9e1fd163d91a39
Armi <arm844630@gmail.com>
2023-11-08 00:12:49 +0800

    perulangan for, while dan Do While

---
+++ Day29.java
@@ -0,0 +1,25 @@
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        // Perulangan for
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // Perulangan while
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        // Perulangan do-while
+        i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+
+}
