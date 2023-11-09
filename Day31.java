552de4c0c36a664d46b378fdec8dcb23db07b8ad
Armi <arm844630@gmail.com>
2023-11-10 01:48:22 +0800

    tampilkan tanggal dan waktu menggunakan variabel

---
+++ Day31.java
@@ -0,0 +1,8 @@
+
+public class Day31 {
+    public static void main(String[] args) {
+        System.out.println("armi");
+       
+    }
+    
+}
---
+++ Day32.java
@@ -0,0 +1,14 @@
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Day32 {
+
+    public static void main(String[] args) {
+
+        LocalDate tanggal = LocalDate.now();
+        LocalTime waktu = LocalTime.now();
+        System.out.println("Tanggal hari ini adalah " + tanggal);
+        System.out.println("Waktu hari ini adalah " + waktu);
+    }
+}
