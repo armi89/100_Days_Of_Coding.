5ab74300e45c329c1dca62de94b957a046e88cd2
Armi <arm844630@gmail.com>
2023-11-13 20:53:44 +0800

    perulangan while

--- Day33.java
+++ Day33.java
@@ -2,7 +2,7 @@
 public class Day33 {
 
     public static void main(String[] args) {
-// Mencetak angka dari 1 hingga 10
+
         int[] angka = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i : angka) {
             System.out.println(i);
