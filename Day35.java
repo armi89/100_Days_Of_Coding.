c9aa1eeed8a5727289944d829426c9bfe0226c5a
Armi <arm844630@gmail.com>
2023-11-13 20:41:57 +0800

    mencetak hasil perkalian setiap angka dengan 5

--- Day35.java
+++ Day35.java
@@ -3,7 +3,7 @@ public class Day35 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 20; i++) {
-            int result = i * 3;
+            int result = i * 5;
             System.out.println(result);
         }
     }
