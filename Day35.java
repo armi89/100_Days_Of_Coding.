6c2cc0bfb916c5e69cd948e990d24b1726ad4929
Armi <arm844630@gmail.com>
2023-11-13 20:30:48 +0800

    mencetak hasil perkalian setiap angka 5

---
+++ Day35.java
@@ -0,0 +1,10 @@
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            int result = i * 3;
+            System.out.println(result);
+        }
+    }
+}
