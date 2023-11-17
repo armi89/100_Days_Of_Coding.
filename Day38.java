7124f0df81ac06d8087084057f375ada8d9b05cd
Armi <arm844630@gmail.com>
2023-11-17 19:01:19 +0800

    mengurutkan daftar nama

---
+++ Day38.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class Day38 {
+
+    public static void main(String[] args) {
+        String[] nama = {"Armi", "obing", "toci"};
+
+        Arrays.sort(nama);
+        for (String n : nama) {
+            System.out.println(n);
+        }
+    }
+
+}
