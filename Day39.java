59f4bd4a3ef390fbf0f3e98429ae02a9dea02db0
Armi <arm844630@gmail.com>
2023-11-18 21:11:59 +0800

    menerima input dari pengguna

---
+++ Day39.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Day39 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("masukkan nama anda");
+        String armi = input.nextLine();
+
+        System.out.println("selamat datang di rumah kami :" + armi);
+
+    }
+}
