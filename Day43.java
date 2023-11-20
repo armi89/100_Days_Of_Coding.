12b0d206259e485994b14c392f86e04d7a7c2d97
Armi <arm844630@gmail.com>
2023-11-20 20:02:32 +0800

    tabel perkalian menggunakan perulangan for

---
+++ Day43.java
@@ -0,0 +1,14 @@
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+
+            }
+            System.out.println();
+        }
+
+    }
+}
