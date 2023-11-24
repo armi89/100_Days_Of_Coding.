0c8aa37deceab98ec8b9e9a036baa3d735daa721
Armi <arm844630@gmail.com>
2023-11-24 18:04:08 +0800

    mencetak bilangan prima

---
+++ Day46.java
@@ -0,0 +1,15 @@
+
+public class Day46 {
+
+    public static void main(String[] args) {
+        int[] bilangan_Prima = {2, 3, 5, 7, 11};
+        System.out.println("bilangan prima  dari 1 sampe 10 adalah sebagai berikut:");
+
+        for (int bilangan : bilangan_Prima) {
+            System.out.println(bilangan + "");
+
+        }
+
+    }
+
+}
