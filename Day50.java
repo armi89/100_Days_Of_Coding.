b311bca070c0f64d9940974375def319fa7f80d2
Armi <arm844630@gmail.com>
2023-11-27 22:00:18 +0800

    Menghitung rata-rata dari dua angka

---
+++ Day50.java
@@ -0,0 +1,11 @@
+
+public class Day50 {
+
+    public static void main(String[] args) {
+        int angka_1 = 10;
+        int angka_2 = 20;
+        int rata_Rata = (angka_1 + angka_2) / 2;
+        System.out.println("Rata-rata dari " + angka_1);
+        System.out.println(" dan " + angka_2 + " adalah " + rata_Rata);
+    }
+}
