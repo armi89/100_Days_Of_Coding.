b9513f7e94d8e5d641b62fe0c3768a1270fd67be
Armi <arm844630@gmail.com>
2023-11-29 22:32:11 +0800

    Menghitung rata-rata dari tiga angka

---
+++ Day51.java
@@ -0,0 +1,13 @@
+
+public class Day51 {
+
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 30;
+        int angka3 = 10;
+        int rataRata = (angka1 + angka2 + angka3) / 3;
+        System.out.println("rata-rata dari" + angka1);
+        System.out.println(+angka2 + "dan" + angka3 + "adalah " + rataRata);
+    }
+
+}
