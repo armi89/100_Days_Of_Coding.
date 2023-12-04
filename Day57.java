fc076245dd00098b52ec0863c49618ef7f524a50
Armi <arm844630@gmail.com>
2023-12-04 20:52:54 +0800

    perulangam break untuk mengnetikan perulangan setelah mencetak angka 5

--- Day57.java
+++ Day57.java
@@ -1,12 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author ArmiA
- */
 public class Day57 {
-    
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+
 }
