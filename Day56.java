64ea824a27a737cbe9e35166cc33d45a909bdcbf
Armi <arm844630@gmail.com>
2023-12-04 20:44:16 +0800

    perulangan for-each untuk mencetak semua elemen dalam array

--- Day56.java
+++ Day56.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author ArmiA
- */
 public class Day56 {
-    
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+    }
 }
